68305d2b943bbb55670aae7f4f319f85d6e0dbd7
Lori Henderson <lorim95@gmail.com>
2022-05-17 21:50:10 -0400

    commit tree 65ad84e102c73a6f17a30e9f9f76350cf6f90792

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(num) {
+    return Math.abs(num - 42);
+}
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination)*264
+}
+
+function calculatesFarePrice(start, destination) {
+    let fare = distanceTravelledInFeet(start, destination);
+
+    if (fare < 400) {
+        return 0;
+    } else if (fare >= 400 && fare <= 2000) {
+        return ((fare - 400) * .02);
+    } else if (fare > 2000 && fare <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
